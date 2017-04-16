977e5dbefa89afedaeb02fd44bcb5a27a3318b5f
Ruben Schuckit <rschuckit@wustl.edu>
2017-04-16 18:51:28 +0200

    Correction to PR algorithm to make sparse matrix using dictionary and accoutning for outlinks that are not keys in the dictionary

--- PageRank/page_rank.py
+++ PageRank/page_rank.py
@@ -1,49 +1,61 @@
 import numpy as np
 import pickle
+
+def get_num_links(link_dict):
+    links_seen = set()
+    count = 0
+
+    for url in link_dict:
+        if url not in links_seen:
+            links_seen.add(url)
+            count = count + 1
+
+        for outlink in link_dict[url]:
+            if outlink not in links_seen:
+                links_seen.add(outlink)
+                count = count + 1
+
+    return count
+
+def create_pr_matrix(data):
+    num_urls = get_num_links(data)
+    matrix = np.zeros([num_urls, num_urls])
+    rows_in_matrix = set([])
+    url_dict = {}
+    url_count = 0
+
+    for url in data.keys():
+        row_num = 0
+        if url in url_dict:
+            row_num = url_dict[url]
+        else:
+            url_dict[url] = url_count
+            row_num = url_count
+            url_count = url_count + 1
+
+        num_outlinks = len(data[url])
+
+        matrix_row = [0 for i in range(num_urls)]
+        cell_value = 1/num_outlinks
+        for outlink in data[url]:
+            col_num = 0
+            if outlink in url_dict:
+                col_num = url_dict[outlink]
+            else:
+                url_dict[outlink] = url_count
+                col_num = url_count
+                url_count = url_count + 1
+
+            matrix_row[col_num] = cell_value
+
+        matrix[row_num] = matrix_row
+        print(url_dict)
+
+    return np.rot90(np.fliplr(matrix))
+
 with open('parent_to_children_urls_0.pickle', 'rb') as f:
     data = pickle.load(f)
 
-data = [['a', ['b', 'c', 'd']], ['b', ['a', 'd']], ['c', ['c']], ['d', ['b', 'c']]]
-num_urls = len(data)
-matrix = np.zeros([num_urls, num_urls])
-rows_in_matrix = set([])
-url_dict = {}
-url_count = 0
+data = {'a': ['b','c','d'], 'b':['a','d'], 'c':['c'], 'd':['b', 'c']}
 
-
-for row in range(len(data)):
-    url = data[row][0]
-
-    # Account for duplicates
-    if url in rows_in_matrix:
-        continue
-
-    row_num = 0
-    if url in url_dict:
-        row_num = url_dict[url]
-    else:
-        url_dict[url] = url_count
-        row_num = url_count
-        url_count = url_count + 1
-
-    num_outlinks = len(data[row][1])
-
-    matrix_row = [0 for i in range(num_urls)]
-    cell_value = 1/num_outlinks
-    for num in range(num_outlinks):
-        url_outlink = data[row][1][num]
-        col_num = 0
-        if url_outlink in url_dict:
-            col_num = url_dict[url_outlink]
-        else:
-            url_dict[url_outlink] = url_count
-            col_num = url_count
-            url_count = url_count + 1
-
-        matrix_row[col_num] = cell_value
-
-    matrix[row_num] = matrix_row
-
-
-print(matrix)
-
+print(create_pr_matrix(data))
